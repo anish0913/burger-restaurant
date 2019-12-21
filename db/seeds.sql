6ba91d2c69c7c7b0306c95850641bfcbc48c30fc
Anish Patel <anishpatel39@gmail.com>
2019-12-21 13:07:33 -0500

    random

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger with Pickles");
+
+INSERT INTO burgers (burger_name) VALUES ("Hamburger no ketcup");
+
+INSERT INTO burgers (burger_name) VALUES ("Cheesebuger no meat");
+
+INSERT INTO burgers (burger_name) VALUES ("Double cheesebuger ");
+
+INSERT INTO burgers (burger_name) VALUES ("Quarter Ponder with ham");
+ 
